c8b39c7d3e47174676609d7555cd5c3c9d58b036
杨凯彬 <ykb553@qq.com>
2019-11-09 21:16:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -31,19 +31,19 @@ new Vue({
                 image: "http://img4.imgtn.bdimg.com/it/u=1037767819,3957627469&fm=26&gp=0.jpg"
             },
             {
-                name: "kaka",
-                url: "1",
-                image: ""
+                name: "Spring",
+                url: "//kail.xyz/Spring",
+                image: "http://img4.imgtn.bdimg.com/it/u=2995354372,2168828254&fm=26&gp=0.jpg"
             },
             {
-                name: "Java",
-                url: "1",
-                image: ""
+                name: "Kafka",
+                url: "//kail.xyz/Kafka",
+                image: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1573315426586&di=6c3c8984607d44e3cb588ccb4ca95817&imgtype=0&src=http%3A%2F%2Fwww.instaclustr.com%2Fwp-content%2Fuploads%2F2017%2F05%2Fapache-kafka-640x640.png"
             },
             {
-                name: "Java",
-                url: "1",
-                image: ""
+                name: "FreeMarker",
+                url: "//kail.xyz/FreeMarker",
+                image: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2725454846,1665261921&fm=26&gp=0.jpg"
             }
         ]
     }
