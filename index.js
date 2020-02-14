c56a89ff3428e90373d0c251910f3676ebe1b116
杨凯彬 <kaibin.yang@ttpai.cn>
2020-02-14 20:17:09 +0800

    fix-for

--- index.js
+++ index.js
@@ -12,17 +12,18 @@ window.onload = function () {
     let datasourceId = document.getElementById("datasouce");
     datasourceId.style.display = "none";
     document.getElementById("app").style.display = "block";
-    
+
+    // let vue={menus:[],projects:[]}
+
     let children = datasourceId.children;
     for (let i = 0; i < children.length; i++) {
         let child = children[i];
         if (child.className == 'menu') {
             let menuList = child.children;
 
+            vue.menus = []
             for (let i = 0; i < menuList.length; i++) {
                 let menu = menuList[i];
-
-                vue.menus = []
                 vue.menus[i] = {
                     icon: menu.getAttribute('data-icon'),
                     href: menu.getAttribute('href'),
@@ -31,13 +32,10 @@ window.onload = function () {
             }
 
         } else if (child.className == 'project') {
-
             let projectList = child.children;
-
+            vue.projects = []
             for (let i = 0; i < projectList.length; i++) {
                 let project = projectList[i];
-
-                vue.projects = []
                 vue.projects[i] = {
                     image: project.getAttribute('data-image'),
                     url: project.getAttribute('href'),
